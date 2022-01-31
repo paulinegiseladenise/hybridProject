c939c7ed87f187b8ece3cf06ff647cf132d44524
Pauline Broängen <90825526+paulinegiseladenise@users.noreply.github.com>
2022-01-31 14:23:24 +0100

    update
    en vecka kvar och senaste uppdateringen

--- App.js
+++ App.js
@@ -1,28 +1,91 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Platform,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  infinite,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RegisterComponent from "./src/RegisterComponent";
+import * as Animatable from "react-native-animatable";
 
 const Stack = createNativeStackNavigator();
 
 function HomeScreen({ navigation }) {
   return (
-   <View>
-      <Text> HomeScreen, Do you want to join the lottery? </Text>
-      <View style={styles.buttons}>
-        <Button color="silver" 
-        title="Register"
-        onPress={() =>
-          navigation.navigate("Register", { id: 82, name: "Benny" })
-        }
-      />
-{/*
-      <Button color="silver"
-        title="Item of the month"
-        onPress={() => navigation.navigate("Item")}
-      />*/}
-    </View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text
+        style={{ fontSize: 18, justifyContent: "center", alignItems: "center" }}
+      >
+        <View>
+          <Animatable.Text
+            animation="flash"
+            
+            iterationCount={infinite}
+          >
+            You can win an
+            exclusive bag!
+          </Animatable.Text>
+        </View>
+      </Text>
+
+      <View style={styles.platformSpecific}>
+        <Text
+          style={{
+            color: "black",
+            fontSize: 16,
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          {"\n"}You register with first and
+          {"\n"}last name and can only
+          {"\n"}register once on each item.
+          {"\n"}
+          {"\n"}Click on the button below to go to
+          {"\n"}the register page.
+          {"\n"}
+          {"\n"}
+        </Text>
+
+        <View style={styles.buttons}>
+          <Button
+            color="#675b51"
+            title="R e g i s t e r"
+            onPress={() =>
+              navigation.navigate("Register" /*{ id: 82, name: "Benny" }*/)
+            }
+          />
+        </View>
+      </View>
+
+      <View style={styles.platformSpecific}>
+        <Text
+          style={{
+            color: "black",
+            fontSize: 16,
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          Click on the button below to see
+          {"\n"}which bag you can win.
+          {"\n"}
+          {"\n"}
+        </Text>
+        <View style={styles.buttons}>
+          <Button
+            color="#675b51"
+            title="B a g   o f   t h e   m o n t h"
+            onPress={() => navigation.navigate("Item")}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -35,8 +98,8 @@ export default function App() {
           name="Home"
           component={HomeScreen}
           options={{
-            title: "Do you want to join the lottery and make a difference at the same time?",
-            headerStyle: { backgroundColor: "#000" },
+            title: "         T H E   B A G   L O T T E R Y",
+            headerStyle: { backgroundColor: "#332d28" },
             headerTintColor: "#fff",
             headerTitleStyle: { fontWeight: "bold" },
           }}
@@ -46,8 +109,19 @@ export default function App() {
           name="Register"
           component={RegisterComponent}
           options={{
-            title: "REGISTER",
-            headerStyle: { backgroundColor: "#000" },
+            headerRight: () => (
+              <Button
+                color="#332d28"
+                title="I n f o"
+                onPress={() =>
+                  alert(
+                    "Bag of the month: 'Snapshot' from MARC JACOBS. Lycka till!"
+                  )
+                }
+              />
+            ),
+            title: "R e g i s t e r   P a g e",
+            headerStyle: { backgroundColor: "#332d28" },
             headerTintColor: "#fff",
             headerTitleStyle: { fontWeight: "bold" },
           }}
@@ -57,8 +131,8 @@ export default function App() {
           name="Item"
           getComponent={() => require("./src/ItemComponents").default}
           options={{
-            title: "ITEM",
-            headerStyle: { backgroundColor: "#000" },
+            title: "B a g   o f   t h e   m o n t h",
+            headerStyle: { backgroundColor: "#332d28" },
             headerTintColor: "#fff",
             headerTitleStyle: { fontWeight: "bold" },
           }}
@@ -69,38 +143,59 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  nameBox: {
-    width: 500,
-    height: 40,
-    backgroundColor: "#FE872D",
-    marginBottom: 10,
+  basicBackground: {
+    width: "90%",
+    height: "40%",
+    backgroundColor: "white",
     padding: 10,
-    fontSize: 14,
-    fontWeight: "bold",
-    borderRadius: 10,
-    alignItems: "center",
-  },
-
-  buttons: {
-    width: 230,
-    height: 40,
-
     margin: 10,
-    padding: 10,
     fontSize: 14,
     fontWeight: "bold",
-    borderRadius: 10,
-    alignItems: "center",
-  },
-});
-
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
   },
+
+  buttons: {
+    margin: 10,
+    padding: 0,
+    fontSize: 14,
+    fontWeight: "bold",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+  platformSpecific: {
+    flex: 1,
+    ...Platform.select({
+      ios: {
+        width: "90%",
+        height: "40%",
+        backgroundColor: "#ffffff",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        margin: 10,
+      },
+      android: {
+        width: "90%",
+        height: "40%",
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        margin: 10,
+      },
+
+      default: {
+        width: "90%",
+        height: "40%",
+        backgroundColor: "#ddd6cf",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20,
+        margin: 10,
+      },
+    }),
+  },
 });
-*/
